680422a8b8347dcd46e788ce92f90c067c700d61
Dranen <mathieu.debonydelavergne@epfl.ch>
2015-03-23 21:44:43 +0100

    Ajustement de certaine unité

--- GraphePol.m
+++ GraphePol.m
@@ -2,4 +2,4 @@ figure
 plot(x2,pol);
 grid on;
 xlabel('x [m]', 'FontSize', 20);
-ylabel('P [V.m^{-5}.kg^{-1}.s^4.A^2]', 'FontSize', 20);
+ylabel('P [C.m^{-3}]', 'FontSize', 20);
--- Grapherhopol.m
+++ Grapherhopol.m
@@ -2,4 +2,4 @@ figure
 plot(x4,rhopol);
 grid on;
 xlabel('x [m]', 'FontSize', 20);
-ylabel('\rho_{polarisation} [C]', 'FontSize', 20);
+ylabel('\rho_{polarisation} [C.m^{-3}]', 'FontSize', 20);
