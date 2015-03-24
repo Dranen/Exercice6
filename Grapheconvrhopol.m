b3fe88ce8d1ccc9e3ef63d5397bc115693fc696b
Dranen <mathieu.debonydelavergne@epfl.ch>
2015-03-24 13:37:25 +0100

    Ajout de graphique suplementaire sur la convergence de rhopol

--- Grapheconvrhopol.m
+++ Grapheconvrhopol.m
@@ -11,6 +11,18 @@ xlabel('N^{-1}', 'FontSize', 20);
 ylabel('$\rho_{polarisation}$ [C.m$^{-3}$]', 'Interpreter', 'latex', 'FontSize', 20);
 
 figure
+plot(log10(1./N.^2),log10(abs(conv_rhopol)));
+grid on;
+xlabel('log(N^{-2})', 'FontSize', 20);
+ylabel('$\log(\rho_{polarisation})$ [C.m$^{-3}$]', 'Interpreter', 'latex', 'FontSize', 20);
+
+figure
+plot(log10(1./N),log10(abs(conv_rhopol)));
+grid on;
+xlabel('log(N^{-1})', 'FontSize', 20);
+ylabel('$\log(\rho_{polarisation})$ [C.m$^{-3}$]', 'Interpreter', 'latex', 'FontSize', 20);
+
+figure
 plot(N,conv_rhopol);
 grid on;
 xlabel('N', 'FontSize', 20);
