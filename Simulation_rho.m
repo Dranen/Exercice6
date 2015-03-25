087bb87c76a29e639d75e9b0f9496e403c020d7d
Dranen <mathieu.debonydelavergne@epfl.ch>
2015-03-25 17:42:04 +0100

    Ajout d'un script pour le cas ou epsilonr = 1

---
+++ Simulation_rho.m
@@ -0,0 +1,19 @@
+clear all;
+N1 = 1000;
+N2 = 1000;
+trivial = 3;
+p = 0;
+output_file = 'out.dat';
+input_file = 'inp.dat';
+
+Simulation;
+Lecture_sortie;
+
+Graphephi;
+GrapheEx;
+GrapheDx;
+Grapherhopol;
+GrapheErr;
+GraphePol;
+
+Suppression_fichier_sortie;
